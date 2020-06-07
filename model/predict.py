e0a828400eae3c00d0d6fb5212dc45982ae8e7ac
ADSmaniotto <ADSmaniotto@gmail.com>
2020-06-06 22:50:59 -0500

    Add img dim vars to predict script + gitignore

--- model/predict.py
+++ model/predict.py
@@ -1,10 +1,11 @@
 import numpy as np
+from model.train import IMG_COLS, IMG_ROWS
 
 
 def reshape_img_upload(img) -> np.ndarray:
-    img = img.resize((28, 28))
+    img = img.resize((IMG_ROWS, IMG_COLS))
     im2arr = np.array(img)
-    im2arr = im2arr.reshape(1, 28, 28, 1)
+    im2arr = im2arr.reshape(1, IMG_ROWS, IMG_COLS, 1)
     return im2arr
 
 
